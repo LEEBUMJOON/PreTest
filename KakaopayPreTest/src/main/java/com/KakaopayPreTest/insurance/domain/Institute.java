ba28eb861a87e0dd5b7e748031d285672c1af1a1
leebj <leebj@notebook-lee>
2020-02-16 11:44:44 +0900

    commit 2

---
+++ KakaopayPreTest/src/main/java/com/KakaopayPreTest/insurance/domain/Institute.java
@@ -0,0 +1,47 @@
+package com.KakaopayPreTest.insurance.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Institute {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 10)
+    private String code;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+
+}
---
+++ KakaopayPreTest/src/main/java/com/KakaopayPreTest/insurance/repository/InstituteRepository.java
@@ -0,0 +1,9 @@
+package com.KakaopayPreTest.insurance.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.KakaopayPreTest.insurance.domain.Institute;
+
+public interface InstituteRepository  extends JpaRepository<Institute, Long>{
+
+}
