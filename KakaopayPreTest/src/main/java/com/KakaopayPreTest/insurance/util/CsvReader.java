1e0890d3d0ca26e00693e7a63a552a006d8ecbad
leebj <leebj@notebook-lee>
2020-02-16 12:33:36 +0900

    commit

--- KakaopayPreTest/src/main/java/com/KakaopayPreTest/insurance/util/CsvReader.java
+++ KakaopayPreTest/src/main/java/com/KakaopayPreTest/insurance/util/CsvReader.java
@@ -3,7 +3,6 @@ package com.KakaopayPreTest.insurance.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
--- KakaopayPreTest/src/test/java/com/KakaopayPreTest/insurance/CsvFileLoadTest/CsvFileUpLoadServiceTest.java
+++ KakaopayPreTest/src/test/java/com/KakaopayPreTest/insurance/CsvFileLoadTest/CsvFileUpLoadServiceTest.java
@@ -4,23 +4,31 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.KakaopayPreTest.insurance.service.CsvFileUpLoadService;
+
 @SpringBootTest
-class CsvFileUpLoadService {
-//	CsvFileUpLoadService csvService =  new CsvFileUpLoadService();
+class CsvFileUpLoadServiceTest {
+
+	CsvFileUpLoadService csvService;
 	
+	@BeforeEach
+	void setUp() throws Exception {
+		csvService =  new CsvFileUpLoadService();	
+	}
+
 	@Test
-	void CsvFileReadTest() {
-		//fail("Not yet implemented");
-//		C:\DevWork\git\repository\KakaopayPreTest\src\main\resources\사전과제3.csv
-		
-		
+	void upLoadtest() throws IOException {
+//		fail("Not yet implemented");		
 		Path path = Paths.get("C:\\DevWork\\git\\repository\\KakaopayPreTest\\src\\main\\resources\\사전과제3.csv");
+
 		String name = "사전과제3";
 		String originalFileName = "사전과제3.csv";
 		String contentType = "text/csv";
@@ -31,11 +39,11 @@ class CsvFileUpLoadService {
 		}
 		MultipartFile result = new MockMultipartFile(name,
 		                     originalFileName, contentType, content);
+			
 		
+		List<String[]> csvData = csvService.upLoad(result);
 		
-		
-		System.out.println(result.getSize());
-		
+		System.out.println(csvData);
 	}
 
 }
