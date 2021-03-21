9c16931a9226b3cad1637a07a50dd9e7abb88140
leebj <leebj@notebook-lee>
2021-03-21 21:08:48 +0900

    COMMIT

---
+++ KakaopayPreTest/src/main/java/com/KakaopayPreTest/insurance/response/dto/IssuanceCouponDto.java
@@ -0,0 +1,19 @@
+package com.KakaopayPreTest.insurance.response.dto;
+
+import com.KakaopayPreTest.insurance.domain.User;
+
+public class IssuanceCouponDto {
+
+	private User issuanceUser;
+
+	public User getIssuanceUser() {
+		return issuanceUser;
+	}
+
+	public void setIssuanceUser(User issuanceUser) {
+		this.issuanceUser = issuanceUser;
+	}
+
+	
+	
+}
